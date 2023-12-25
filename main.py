bf3574606edbe6da8632bb4f2cf2aa001caec228
mistarille224 <bb108396@GMAIL.COM>
2023-12-25 11:39:43 +0800

    modified:   .gitignore modified:   main.py modified:   watcher.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from time import sleep
 import logging
-from watchdog.observers import Observer
-from log import log_path,today_date,clean_log
-from watcher import MyHandler
-from tray import create_tray_icon
 from pathlib import Path
-from conf import data,create_conf
+from conf import create_conf
+from log import log_path,today_date,clean_log
+from tray import create_tray_icon
+from watcher import watch
 
+     
 # 创建配置文件
 create_conf()
 
@@ -22,21 +21,5 @@ clean_log()
 
 
 if __name__ == '__main__':
-    
-    # 监测的文件夹路径
-    input_path = rf'{data["path"]["input_path"]}'
-    # 创建Observer对象
-    observer = Observer()
-    # 创建事件处理对象
-    event_handler = MyHandler()
-    # 将事件处理对象注册到Observer对象中
-    observer.schedule(event_handler, input_path, True)
-    # 启动Observer对象
-    observer.start()
     create_tray_icon()
-
-    try:
-        while True:
-            sleep(20)
-    except KeyboardInterrupt:
-        observer.stop()
+    watch()
--- watcher.py
+++ watcher.py
@@ -1,6 +1,9 @@
 import logging
 from rename import rename
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+from time import sleep
+from conf import data
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -15,3 +18,20 @@ class MyHandler(FileSystemEventHandler):
         # 处理文件修改事件
         if '.' in event.src_path:
             logging.info(f'File modified: {event.src_path}')
+    
+def watch():
+    # 监测的文件夹路径
+    input_path = rf'{data["path"]["input_path"]}'
+    # 创建Observer对象
+    observer = Observer()
+    # 创建事件处理对象
+    event_handler = MyHandler()
+    # 将事件处理对象注册到Observer对象中
+    observer.schedule(event_handler, input_path, True)
+    # 启动Observer对象
+    observer.start()
+    try:
+        while True:
+            sleep(20)
+    except KeyboardInterrupt:
+        observer.stop()
