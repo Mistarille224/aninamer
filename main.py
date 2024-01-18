36e18bae609ffb976d8f29cd467fc44603846449
mistarille224 <bb108396@GMAIL.COM>
2024-01-18 21:02:45 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from conf import create_conf
 from log import create_log,clean_log
 from tray import create_tray_icon
 from watcher import watch
+from tendo import singleton
 
+me = singleton.SingleInstance()
      
 # 创建配置文件
 create_conf()
