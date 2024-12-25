256ed077ba51e68d612883c2e3e399b57157f6a5
mistarille224 <bb108396@GMAIL.COM>
2024-12-25 10:55:40 +0800

    avoid renaming file that without '[]'

--- tree.py
+++ tree.py
@@ -20,23 +20,24 @@ def save_tree(tree, path):
             os.fsync(f.fileno())
 
 def apply_rules(name):
-    if name[name.find("]") + 1] == "[":
-        name = name[:name.find("]") + 1] + name[name.find("]") + 2:]
-    name = re.sub(r'\[(\d+)\]', r' E\1 ', name)
-    name = re.sub(r' (\d+) ', r' E\1 ', name)
-    name = re.sub(r'(\d+)-(\d+)', r' E\1-E\2 ', name)
-    name = name.replace(' - ', ' ').replace('[v2]', ' - v2 ')
-    name = re.sub(r'\[(\d+)v2\]', r' E\1 - v2 ', name)
-    name = re.sub(r'\[ E(\d+)', r' E\1', name)
-    name = re.sub(r'\[[^]]*\]', '', name).replace(']', '')
-    if len(re.findall(r' E(\d+)', name)) > 1:
-        if '1080' in re.findall(r' E(\d+)', name):
-            name =  name.replace('E1080', '')
-        if '720' in re.findall(r' E(\d+)', name):
-            name =  name.replace('E720', '')
-        for match in re.findall(r' E(\d+)', name)[:-1]:
-            name = re.sub(r'E'+match,match,name,count=1)
-    name = re.sub(r'\s+', ' ', name).replace(' .', '.').strip()
+    if ']' in name:
+        if name[name.find(']') + 1] == '[':
+            name = name[:name.find(']') + 1] + name[name.find(']') + 2:]
+        name = re.sub(r'\[(\d+)\]', r' E\1 ', name)
+        name = re.sub(r' (\d+) ', r' E\1 ', name)
+        name = re.sub(r'(\d+)-(\d+)', r' E\1-E\2 ', name)
+        name = name.replace(' - ', ' ').replace('[v2]', ' - v2 ')
+        name = re.sub(r'\[(\d+)v2\]', r' E\1 - v2 ', name)
+        name = re.sub(r'\[ E(\d+)', r' E\1', name)
+        name = re.sub(r'\[[^]]*\]', '', name).replace(']', '')
+        if len(re.findall(r' E(\d+)', name)) > 1:
+            if '1080' in re.findall(r' E(\d+)', name):
+                name =  name.replace('E1080', '')
+            if '720' in re.findall(r' E(\d+)', name):
+                name =  name.replace('E720', '')
+            for match in re.findall(r' E(\d+)', name)[:-1]:
+                name = re.sub(r'E'+match,match,name,count=1)
+        name = re.sub(r'\s+', ' ', name).replace(' .', '.').strip()
     return name
 
 def build_tree(last_conf):
